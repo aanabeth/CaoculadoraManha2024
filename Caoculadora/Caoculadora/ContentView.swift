b3f142a75ade72257758473f721be0cd60d1ef2b
aanabeth <aanabeth@MacBook-Pro-77.local>
2024-05-13 11:59:47 -0300

    Cria enum de Porte, muda logica de criacao do picker e implementa style guide para fontes

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,68 +11,104 @@ struct ContentView: View {
     
     @State var anos: Int? = nil
     @State var meses: Int? = nil
-    @State private var selectedSegment = 0
+//    @State var selectedSegment = 0
+    @State var selectedPorte = Porte.pequeno
     @State var result: Int?  = nil
     
     var body: some View {
         VStack(alignment: .leading) {
             Text("Qual a idade do seu cão?")
+                .font(.h5)
             Text("Anos")
+                .font(.b1)
             TextField(
                 "Quantos anos completos tem seu cão?",
                 value: $anos,
                 format: .number
             )
             
+            
             Text("Meses")
+                .font(.b1)
             TextField(
                 "Quantos meses além disso ele tem?",
                 value: $meses,
                 format: .number
             )
             
+            
             Text("Porte")
+                .font(.b1)
             
             
-            Picker("Options", selection: $selectedSegment) {
-                Text("Pequeno").tag(0)
-                Text("Médio").tag(1)
-                Text("Grande").tag(2)
+            Picker("Options", selection: $selectedPorte) {
+                ForEach(Porte.allCases, id:\.self) { porte in
+                    Text(porte.rawValue)
                 }
+//                Text("Pequeno").tag(0)
+//                Text("Médio").tag(1)
+//                Text("Grande").tag(2)
+            }
+            
+            Divider()
             
             if let result {
                 Text("Seu cão tem, em idade humana...")
+                    .font(.b1)
                 Text("\(result) anos")
+                    .font(.display)
             } else {
                 Image(ImageResource.clarinha)
                     .resizable()
                     .scaledToFit()
                     .frame(maxHeight: 150)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    . shadow(radius: 5)
+            }
+            
+            Button("Cãocular", action: processYears)
+                .font(.b1)
+            
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            }
-
-            Button("Cãocular") {
-                print("Cãocular")
-                result = 23
-                
-            }
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            .frame(height: 50)
-            .background(.indigo)
-            .foregroundStyle(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .bold()
+                .frame(height: 50)
+                .background(.indigo)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
-            }
-//            .pickerStyle(.wheel)
-            .pickerStyle(.palette)
-            .textFieldStyle(.roundedBorder)
-            .keyboardType(.numberPad)
-            .padding()
-            
-        
         }
+        //            .pickerStyle(.wheel)
+        .pickerStyle(.palette)
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
+        .padding()
+        
     }
+    
+    func processYears() {
+        print("Cãocular")
+        
+        guard let anos, let meses else {
+            print("campos não preenchidos")
+            return
+        }
+        
+        guard meses > 0 || anos > 0 else {
+            print("Pelo menos um campo deve ser maior que zero")
+            return
+        }
+        
+        let multiplicador: Int
+        switch selectedPorte {
+        case .pequeno:
+            multiplicador = 6
+        case .médio:
+            multiplicador = 7
+        case .grande:
+            multiplicador = 8
+        }
+        result = anos * multiplicador + meses * multiplicador/12
+    }
+}
 
 #Preview {
     ContentView()
---
+++ Caoculadora/Porte.swift
@@ -0,0 +1,12 @@
+//
+//  Porte.swift
+//  Caoculadora
+//
+//  Created by Ana Elizabeth Bastos de Mendonça on 13/05/24.
+//
+
+import Foundation
+
+enum Porte: String, CaseIterable {
+    case pequeno = "Pequeno", médio = "Médio", grande = "Grande"
+}
