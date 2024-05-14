a37f2366845b3f8ead3bf8bb79215a595c53d2a7
aanabeth <aanabeth@MacBook-Pro-77.local>
2024-05-14 11:03:28 -0300

    Customizacao da navigation bar e add scrool view

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,78 +11,95 @@ struct ContentView: View {
     
     @State var anos: Int? = nil
     @State var meses: Int? = nil
-//    @State var selectedSegment = 0
+    //    @State var selectedSegment = 0
     @State var selectedPorte = Porte.pequeno
     @State var result: Int?  = nil
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Qual a idade do seu cão?")
-                .font(.h5)
-            Text("Anos")
-                .font(.b1)
-            TextField(
-                "Quantos anos completos tem seu cão?",
-                value: $anos,
-                format: .number
-            )
-            
-            
-            Text("Meses")
-                .font(.b1)
-            TextField(
-                "Quantos meses além disso ele tem?",
-                value: $meses,
-                format: .number
-            )
-            
-            
-            Text("Porte")
-                .font(.b1)
-            
-            
-            Picker("Options", selection: $selectedPorte) {
-                ForEach(Porte.allCases, id:\.self) { porte in
-                    Text(porte.rawValue)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Qual a idade do seu cão?")
+                        .foregroundStyle(.indigo60)
+                        .font(.h5)
+                    Text("Anos")
+                        .foregroundStyle(.indigo60)
+                        .font(.b1)
+                    TextField(
+                        "Quantos anos completos tem seu cão?",
+                        value: $anos,
+                        format: .number
+                    )
+                    Spacer()
+                    
+                    Text("Meses")
+                        .foregroundStyle(.indigo60)
+                        .font(.b1)
+                    TextField(
+                        "Quantos meses além disso ele tem?",
+                        value: $meses,
+                        format: .number
+                    )
+                    Spacer()
+                    
+                    Text("Porte")
+                        .foregroundStyle(.indigo60)
+                        .font(.b1)
+                    
+                    
+                    Picker("Options", selection: $selectedPorte) {
+                        ForEach(Porte.allCases, id:\.self) { porte in
+                            Text(porte.rawValue)
+                        }
+                        //                Text("Pequeno").tag(0)
+                        //                Text("Médio").tag(1)
+                        //                Text("Grande").tag(2)
+                    }
+                    
+                    Divider()
+                    Spacer()
+                    if let result {
+                        Text("Seu cão tem, em idade humana...")
+                            .font(.b1)
+                        Text("\(result) anos")
+                            .font(.display)
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            . shadow(radius: 5)
+                    }
+                    Spacer()
+                    Button("Cãocular", action: processYears)
+                        .font(.b1)
+                    
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.indigo)
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
                 }
-//                Text("Pequeno").tag(0)
-//                Text("Médio").tag(1)
-//                Text("Grande").tag(2)
+                //            .pickerStyle(.wheel)
+                .pickerStyle(.palette)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .padding()
+                .containerRelativeFrame(.vertical)
             }
-            
-            Divider()
-            
-            if let result {
-                Text("Seu cão tem, em idade humana...")
-                    .font(.b1)
-                Text("\(result) anos")
-                    .font(.display)
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    . shadow(radius: 5)
-            }
-            
-            Button("Cãocular", action: processYears)
-                .font(.b1)
-            
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .frame(height: 50)
-                .background(.indigo)
-                .foregroundStyle(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-            
+            .navigationTitle("Cãoculadora")
+            .scrollDismissesKeyboard(.immediately)
+            .toolbarBackground(.indigo, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar)
         }
-        //            .pickerStyle(.wheel)
-        .pickerStyle(.palette)
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .padding()
-        
+        .fontDesign(.rounded)
     }
+}
+
+extension ContentView {
     
     func processYears() {
         print("Cãocular")
@@ -99,6 +116,7 @@ struct ContentView: View {
         
         result = selectedPorte.calcularIdade(deAnos: anos, eMeses: meses)
     }
+    
 }
 
 #Preview {
